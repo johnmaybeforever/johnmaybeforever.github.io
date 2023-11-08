4993cac0e3e5e4d31b87db16cf97708bcb510ea3
John M <unused>
2023-11-08 13:08:54 -0500

    wip add slip on

---
+++ src/app/slip-on-boots/slip-on-boots.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+// TODO
+@Component({
+  selector: 'app-slip-on-boots',
+  templateUrl: './slip-on-boots.component.html',
+  styleUrls: ['./slip-on-boots.component.css']
+})
+export class SlipOnBootsComponent {
+
+}
